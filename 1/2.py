08352de1146776476ff9845830646b237e013a5a
pingumen96 <sinhue.rossi@gmail.com>
2023-01-09 23:33:16 +0100

    mnist

---
+++ 1/2.py
@@ -0,0 +1,10 @@
+import tensorflow as tf
+from tensorflow import keras
+
+NB_CLASSES = 10
+RESHAPED = 784
+
+model = tf.keras.models.Sequential()
+model.add(keras.layers.Dense(NB_CLASSES,
+                             input_shape=(RESHAPED,), kernel_initializer='zeros',
+                             name='dense_layer', activation='softmax'))
